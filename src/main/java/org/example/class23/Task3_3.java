6c7be511a39545255a0b616d7dfafd6c5d96dfa0
Asghar <instructor@syntaxtechs.com>
2024-04-17 22:04:22 -0400

    class 24

--- src/main/java/org/example/class23/Task3_3.java
+++ src/main/java/org/example/class23/Task3_3.java
@@ -5,23 +5,15 @@ import java.util.ArrayList;
 /*
 
  */
-public class Task3 {
+public class Task3_3 {
     public static void main(String[] args) {
         ArrayList<String> words=new ArrayList();
         words.add("Cat");
         words.add("Dog");
         words.add("Home");
         words.add("Place");
-
-
-        for(int i=0; i<words.size();i++){
-            String word=words.get(i);
-            if(word.endsWith("e")){
-                words.remove(i);
-
-            }
-
-        }
+        words.removeIf(x->x.endsWith("e"));
         System.out.println(words);
+        words.forEach(x-> System.out.println(x));
     }
 }
