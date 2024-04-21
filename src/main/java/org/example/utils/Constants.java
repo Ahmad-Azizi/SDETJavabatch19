f8fe44183f86da67fddfbbb35deca6dde8eaf312
Asghar <instructor@syntaxtechs.com>
2024-04-21 14:59:00 -0400

    Class 26

---
+++ src/main/java/org/example/utils/Constants.java
@@ -0,0 +1,5 @@
+package org.example.utils;
+
+public class Constants {
+    public static final String CONFIG_FILE_PATH="C:\\Users\\Syntax\\IdeaProjects\\SDETJavaBatch19\\Files\\Config.properties";
+}
